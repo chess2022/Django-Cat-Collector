03ea26c8290c8311b8a2e96ebd10cadba20e2eab
chess2022 <lakeshore.hnh@gmail.com>
2022-07-05 20:50:24 -0700

    Defines home view in views.py

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+# Add the import;  in order to use the HttpResponsefunction, we must import it like the others we've used so far.
+from django.http import HttpResponse
+
+# Define the home view
+def home(request):
+    return HttpResponse('<h1>Hello World /ᐠ｡‸｡ᐟ\ﾉ</h1>')
